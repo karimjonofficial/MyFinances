724add06c92027b454aeae0036711559325dce4d
Karimjon <okracer3350@gmail.com>
2025-11-23 16:32:26 +0500

    Try to write a text field with a double state with formatting with spaces

--- app/src/main/java/com/orka/myfinances/MainActivity.kt
+++ app/src/main/java/com/orka/myfinances/MainActivity.kt
@@ -31,3 +31,45 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+/**@Preview(
+    showSystemUi = true,
+    showBackground = true
+)
+@Composable
+private fun TextFieldTransformations() {
+//                onValueChange = {
+//                    val doubleOrNull = it.parseFromFormatted()
+//                    double.value = doubleOrNull ?: 0.0
+//                    text.value = if(it.isBlank()) it else doubleOrNull?.format() ?: "0"
+//                },
+//                maxLines = 1,
+
+    MyFinancesTheme {
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            val double = rememberSaveable { mutableStateOf(0.0) }
+            val text = rememberSaveable { mutableStateOf(double.value.format()) }
+            val state = rememberTextFieldState()
+
+            TextField(
+                state = state,
+                label = { Text(text = stringResource(R.string.price)) },
+                lineLimits = TextFieldLineLimits.SingleLine,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Decimal
+                ),
+                outputTransformation = OutputTransformation {
+                    val d = originalText.toString().toDoubleOrNull()
+                    
+                    if(d != null) {
+
+                    }
+                }
+            )
+        }
+    }
+}**/
