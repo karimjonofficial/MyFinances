729d69c7adb76b414ec8c726db4d6dd7787dce95
Karimjon <okracer3350@gmail.com>
2025-12-06 09:18:17 +0500

    Cover classes with tests

--- app/src/test/java/com/orka/myfinances/data/repositories/basket/BasketRepositoryTest.kt
+++ app/src/test/java/com/orka/myfinances/data/repositories/basket/BasketRepositoryTest.kt
@@ -5,9 +5,9 @@ import com.orka.myfinances.data.repositories.product.ProductRepository
 import com.orka.myfinances.fixtures.data.api.product.ProductApiServiceStub
 import com.orka.myfinances.testLib.amount
 import com.orka.myfinances.testLib.id1
+import com.orka.myfinances.testLib.product2
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -19,8 +19,8 @@ class BasketRepositoryTest : MainDispatcherContext() {
 
     @Test
     fun `Returns an empty basket`() {
-        val basket = repository.get()
-        assertTrue(basket.isEmpty())
+        val items = repository.get()
+        assertTrue(items.isEmpty())
     }
 
     @Nested
@@ -29,13 +29,22 @@ class BasketRepositoryTest : MainDispatcherContext() {
         fun setup() {
             testScope.launch {
                 repository.add(id1, amount)
+                advanceUntilIdle()
             }
         }
 
         @Test
-        fun `Add items adds items`() = testScope.runTest {
+        fun `Add items adds items`() = runTest {
             val items = repository.get()
             assertTrue(items.find { it.product.id == id1 } != null && items.size == 1)
+        }
+
+        @Test
+        fun `Nothing happens when item does not exist`() {
+            val old = repository.get()
+            repository.remove(product2.id, amount)
+            val new = repository.get()
+            assertEquals(old, new)
         }
 
         @Nested
@@ -44,11 +53,12 @@ class BasketRepositoryTest : MainDispatcherContext() {
             fun setup() {
                 testScope.launch {
                     repository.add(id1, amount)
+                    advanceUntilIdle()
                 }
             }
 
             @Test
-            fun `Add increases when item already exists`() = testScope.runTest {
+            fun `Add increases when item already exists`() = runTest {
                 val items = repository.get()
                 assertTrue(
                     items.find { it.product.id == id1 } != null
@@ -58,7 +68,7 @@ class BasketRepositoryTest : MainDispatcherContext() {
             }
 
             @Test
-            fun `Remove decreases when item already exists`() = testScope.runTest {
+            fun `Remove decreases when item already exists`() = runTest {
                 repository.remove(id1, amount)
                 val items = repository.get()
                 assertTrue(
@@ -70,7 +80,7 @@ class BasketRepositoryTest : MainDispatcherContext() {
         }
 
         @Test
-        fun `Remove removes when item already exists and amount is 1`() = testScope.runTest {
+        fun `Remove removes when item already exists and amount is 1`() = runTest {
             repository.remove(id1, amount)
             val items = repository.get()
             assertTrue(items.find { it.product.id == id1 } == null)
