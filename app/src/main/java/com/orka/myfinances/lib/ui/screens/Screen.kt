9d40cd1858f65f774f9ce0981ecc0ffb6eafbd7e
Karimjon <okracer3350@gmail.com>
2025-12-06 11:38:41 +0500

    Add spacing and align things properly

--- app/src/main/java/com/orka/myfinances/lib/ui/screens/Screen.kt
+++ app/src/main/java/com/orka/myfinances/lib/ui/screens/Screen.kt
@@ -1,9 +1,13 @@
 package com.orka.myfinances.lib.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.orka.myfinances.lib.ui.viewmodel.ListViewModel
 import com.orka.myfinances.lib.ui.viewmodel.State
 
@@ -12,7 +16,7 @@ fun <TLoading, TSuccess, TFailure> Screen(
     modifier: Modifier = Modifier,
     state: State<TLoading, List<TSuccess>, TFailure>,
     viewModel: ListViewModel<TLoading, TSuccess, TFailure>,
-    item: @Composable (TSuccess) -> Unit
+    item: @Composable (modifier: Modifier, item: TSuccess) -> Unit
 ) {
     when(state) {
         is State.Initial -> { LoadingScreen(modifier) }
@@ -27,9 +31,13 @@ fun <TLoading, TSuccess, TFailure> Screen(
 
         is State.Success -> {
 
-            LazyColumn(modifier = modifier) {
+            LazyColumn(
+                modifier = modifier,
+                contentPadding = PaddingValues(horizontal = 4.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
                 items(items = state.value) {
-                    item(it)
+                    item(Modifier.fillMaxWidth(), it)
                 }
             }
         }
