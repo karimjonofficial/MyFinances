a78ebfdf15a6c508cef35d0fc16a2ad6498e8f81
Jamoliddin <jamoliddin0968@gmail.com>
2026-03-14 22:51:29 +0500

    Add RoundedCornerShape import in DebtScreen and NavigationBar imports in ProfileContent

--- app/src/main/java/com/orka/myfinances/ui/screens/home/ProfileContent.kt
+++ app/src/main/java/com/orka/myfinances/ui/screens/home/ProfileContent.kt
@@ -18,6 +18,8 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
