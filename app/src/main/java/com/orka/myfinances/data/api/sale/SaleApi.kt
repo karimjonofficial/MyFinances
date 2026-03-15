29f8a84a4babef76613ddb4363c8ba66af03dc37
Karimjon <okracer3350@gmail.com>
2026-03-15 14:49:05 +0500

    Filter by branch

--- app/src/main/java/com/orka/myfinances/data/api/sale/SaleApi.kt
+++ app/src/main/java/com/orka/myfinances/data/api/sale/SaleApi.kt
@@ -6,6 +6,7 @@ import com.orka.myfinances.data.repositories.sale.SaleEvent
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.HttpStatusCode
@@ -17,7 +18,10 @@ class SaleApi(
     private val flow: MutableSharedFlow<SaleEvent>
 ) {
     suspend fun getAll(): List<SaleApiModel>? {
-        val response = client.get("sales/")
+        val response = client.get(
+            urlString = "sales/",
+            block = { parameter("branch", office.id.value) }
+        )
         return if (response.status == HttpStatusCode.OK) response.body() else null
     }
 
@@ -32,7 +36,7 @@ class SaleApi(
             block = { setBody(request.map(office.id.value)) }
         )
         val created = response.status == HttpStatusCode.Created
-        if(created) flow.emit(SaleEvent)
-        return if(created) response.body() else null
+        if (created) flow.emit(SaleEvent)
+        return if (created) response.body() else null
     }
 }
