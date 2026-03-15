02ea8ac1af8a72e0570e6f1b15695c6b5b8a0a32
Karimjon <okracer3350@gmail.com>
2026-03-15 14:52:20 +0500

    Removing unnecessary field

--- app/src/main/java/com/orka/myfinances/data/api/receive/ProductTitleMinimalApiModel.kt
+++ app/src/main/java/com/orka/myfinances/data/api/receive/ProductTitleMinimalApiModel.kt
@@ -1,11 +1,9 @@
 package com.orka.myfinances.data.api.receive
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ProductTitleMinimalApiModel(
     val id: Int,
-    @SerialName("category") val categoryId: Int,
     val name: String
 )
