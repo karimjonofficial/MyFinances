ac13704e0d9b3b3c71a1ecac1947a0414813e968
Karimjon <okracer3350@gmail.com>
2026-03-24 18:05:11 +0500

    Add swipe-to-switch to screens with tabs

--- app/src/main/java/com/orka/myfinances/lib/ui/screens/MultipleTabScreen.kt
+++ app/src/main/java/com/orka/myfinances/lib/ui/screens/MultipleTabScreen.kt
@@ -2,20 +2,24 @@ package com.orka.myfinances.lib.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import com.orka.myfinances.lib.extensions.ui.scaffoldPadding
 import com.orka.myfinances.lib.ui.Scaffold
 import com.orka.myfinances.lib.ui.models.ScreenTab
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MultipleTabScreen(
     modifier: Modifier = Modifier,
@@ -24,28 +28,38 @@ fun MultipleTabScreen(
 ) {
     Scaffold(
         modifier = modifier,
-        topBar = {
-            TopAppBar(
-                title = { Text(text = title) }
-            )
-        }
+        title = title,
     ) { paddingValues ->
-
         Column(modifier = Modifier.scaffoldPadding(paddingValues)) {
             val selectedTab = rememberSaveable { mutableIntStateOf(0) }
-            val selectedTabValue = selectedTab.intValue
+            val pagerState = rememberPagerState(pageCount = { tabs.size })
+            val coroutineScope = rememberCoroutineScope()
+            LaunchedEffect(pagerState) {
+                snapshotFlow { pagerState.currentPage }
+                    .collect { page ->
+                        selectedTab.intValue = page
+                    }
+            }
 
-            PrimaryTabRow(selectedTabIndex = selectedTabValue) {
+            PrimaryTabRow(selectedTabIndex = selectedTab.intValue) {
                 tabs.forEach {
                     Tab(
-                        selected = selectedTabValue == it.index,
-                        onClick = { selectedTab.intValue = it.index },
+                        selected = selectedTab.intValue == it.index,
+                        onClick = {
+                            coroutineScope.launch { pagerState.animateScrollToPage(it.index) }
+                            selectedTab.intValue = it.index
+                        },
                         text = { Text(text = it.title) }
                     )
                 }
             }
 
-            tabs[selectedTabValue].content?.invoke(Modifier.fillMaxSize())
+            HorizontalPager(
+                modifier = Modifier.weight(1f).fillMaxWidth(),
+                state = pagerState,
+            ) { page ->
+                tabs[page].content?.invoke(Modifier.fillMaxSize())
+            }
         }
     }
 }
