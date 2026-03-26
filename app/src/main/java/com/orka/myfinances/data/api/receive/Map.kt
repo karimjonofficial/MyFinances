519b92bc8cfc6c2b0a2b02f963a9a97b7ea3673a
Karimjon <okracer3350@gmail.com>
2026-03-26 10:35:07 +0500

    Refactor and fix issues in the CategoryScreen

--- app/src/main/java/com/orka/myfinances/data/api/receive/Map.kt
+++ app/src/main/java/com/orka/myfinances/data/api/receive/Map.kt
@@ -2,15 +2,8 @@ package com.orka.myfinances.data.api.receive
 
 import com.orka.myfinances.data.api.receive.models.request.AddReceiveApiRequest
 import com.orka.myfinances.data.api.receive.models.request.AddReceiveApiRequestItem
-import com.orka.myfinances.data.api.receive.models.response.ReceiveApiModel
-import com.orka.myfinances.data.models.Id
 import com.orka.myfinances.data.repositories.receive.AddReceiveRequest
 import com.orka.myfinances.data.repositories.receive.AddReceiveRequestItem
-import com.orka.myfinances.lib.format.FormatDecimal
-import com.orka.myfinances.lib.format.FormatPrice
-import com.orka.myfinances.lib.format.FormatTime
-import com.orka.myfinances.ui.screens.receive.list.components.ReceiveCardModel
-import com.orka.myfinances.ui.screens.receive.list.viewmodel.ReceiveUiModel
 
 fun AddReceiveRequest.map(officeId: Int): AddReceiveApiRequest {
     return AddReceiveApiRequest(
