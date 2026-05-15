c4136f043a3715e445daa5877c01c909fb26d124
Karimjon <okracer3350@gmail.com>
2026-05-15 16:18:02 +0500

    Limit max lines in OptionButton

--- app/src/main/java/com/orka/myfinances/ui/screens/folder/home/OptionButton.kt
+++ app/src/main/java/com/orka/myfinances/ui/screens/folder/home/OptionButton.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import com.orka.myfinances.lib.ui.components.HorizontalSpacer
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
@@ -37,6 +38,8 @@ fun OptionButton(
         Text(
             text = text,
             style = MaterialTheme.typography.bodyLarge,
+            softWrap = false,
+            overflow = TextOverflow.Ellipsis,
             maxLines = 1,
             fontWeight = FontWeight.Bold
         )
