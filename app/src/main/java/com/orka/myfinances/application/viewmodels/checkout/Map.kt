6793042c32cb54e641338adb5e8d2ef16c8c421a
Karimjon <okracer3350@gmail.com>
2026-05-15 15:52:07 +0500

    Use exposedPrice in viewmodels. Use ListItem in OrdersHistoryContent

--- app/src/main/java/com/orka/myfinances/application/viewmodels/checkout/Map.kt
+++ app/src/main/java/com/orka/myfinances/application/viewmodels/checkout/Map.kt
@@ -1,15 +1,9 @@
 package com.orka.myfinances.application.viewmodels.checkout
 
-import com.orka.myfinances.data.models.Id
-import com.orka.myfinances.data.models.basket.Basket
 import com.orka.myfinances.data.models.basket.BasketItem
-import com.orka.myfinances.data.repositories.order.AddOrderRequest
-import com.orka.myfinances.data.repositories.sale.AddSaleRequest
-import com.orka.myfinances.lib.data.models.Item
 import com.orka.myfinances.lib.format.FormatDecimal
 import com.orka.myfinances.lib.format.FormatPrice
 import com.orka.myfinances.ui.screens.checkout.viewmodel.BasketItemCardModel
-import kotlin.time.Instant
 
 fun BasketItem.toModel(formatPrice: FormatPrice, formatDecimal: FormatDecimal): BasketItemCardModel {
     return BasketItemCardModel(
