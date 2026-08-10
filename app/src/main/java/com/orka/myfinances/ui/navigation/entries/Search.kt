6166153554982afed54bc3609881fdc794405946
Karimjon <okracer3350@gmail.com>
2026-08-10 19:09:02 +0500

    Add keys to the viewModel composables. Fix issues related to search. Change SettingsButton.

--- app/src/main/java/com/orka/myfinances/ui/navigation/entries/Search.kt
+++ app/src/main/java/com/orka/myfinances/ui/navigation/entries/Search.kt
@@ -2,7 +2,6 @@ package com.orka.myfinances.ui.navigation.entries
 
 import androidx.compose.ui.Modifier
 import androidx.navigation3.runtime.NavEntry
-import com.orka.myfinances.data.models.Session
 import com.orka.myfinances.lib.ui.entry.entry
 import com.orka.myfinances.ui.navigation.destination.Destination
 import com.orka.myfinances.ui.screens.search.SearchScreen
